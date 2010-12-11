5f913d9d3ffdda10efda5f23e602e9e3c59c1078
Miller Hooks <me@millerhooks.com>
2010-12-10 23:19:30 -0600

    models have fields. YAY\!

--- projects/models.py
+++ projects/models.py
@@ -13,26 +13,26 @@ DECK = (
 )
 
 class UserProfile(model.Model):
-    avatar
-    bio
-    title
+    avatar  = models.FileField(upload_to="dynamic/files", blank=True)
+    bio     = models.TextField(blank=True)
+    title   = models.CharField(max_length=255)
 
 class Project(model.Model):
     owner       = models.ForeignKey(User)
     managers    = models.ManyToManyField(User)
-    description
-    tags
+    description = models.TextField(blank=True)
+    #tags
     
 class Story(model.Model):
-    story
-    description
+    story           = models.ForeignKey(Story)
+    description     = models.TextField(blank=True)
     project         = models.ForeignKey(Project)
     developers      = models.ManyToManyField(User)
     owner           = models.ForeignKey(User)
 
 class PlayedCard(model.Model):
-    story
-    player
+    story   = models.ForeignKey(Story)
+    player  = models.ForeignKey(User)
     card    = models.CharField(max_length=1, choices=DECK)
 
 # Create your models here.
